59432273c820fb72a379a9748bd59e91bc852b7f
Michael Dawson <m.dawson@y7mail.com>
2025-04-05 13:59:24 -0500

    Add id to rsvps

--- lib/schemas/rsvp.d.ts
+++ lib/schemas/rsvp.d.ts
@@ -1,9 +1,11 @@
 import * as yup from "yup";
 export declare const rsvpSchema: yup.ObjectSchema<{
+    id: string | undefined;
     userId: string;
     walkId: string;
     timestamp: import("./utils/firebase").Timestamp | undefined;
 }, yup.AnyObject, {
+    id: undefined;
     userId: undefined;
     walkId: undefined;
     timestamp: undefined;
--- src/schemas/rsvp.ts
+++ src/schemas/rsvp.ts
@@ -2,6 +2,7 @@ import * as yup from "yup";
 import { timestampSchema } from "./utils/timestamp";
 
 export const rsvpSchema = yup.object({
+  id: yup.string(),
   userId: yup.string().required(),
   walkId: yup.string().required(),
   timestamp: timestampSchema,
