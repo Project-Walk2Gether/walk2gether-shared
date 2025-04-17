a3d87fec39168f193aafda851b3ea8469c1346d6
Michael Dawson <m.dawson@y7mail.com>
2025-04-17 17:51:41 -0500

    Update participant type

--- lib/schemas/participant.d.ts
+++ lib/schemas/participant.d.ts
@@ -1,16 +1,30 @@
 import * as yup from "yup";
 export declare const participantSchema: yup.ObjectSchema<{
+    id: string | undefined;
     userUids: string[];
     displayName: string;
     photoURL: string | undefined;
-    lastLocation: {} | undefined;
+    lastLocation: {
+        latitude: number;
+        longitude: number;
+        timestamp: number;
+    };
+    approvedAt: import("./utils/firebase").Timestamp | null | undefined;
+    rejectedAt: import("./utils/firebase").Timestamp | undefined;
     createdAt: import("./utils/firebase").Timestamp | undefined;
     updatedAt: import("./utils/firebase").Timestamp | undefined;
 }, yup.AnyObject, {
+    id: undefined;
     userUids: "";
     displayName: undefined;
     photoURL: undefined;
-    lastLocation: {};
+    lastLocation: {
+        latitude: undefined;
+        longitude: undefined;
+        timestamp: undefined;
+    };
+    approvedAt: undefined;
+    rejectedAt: undefined;
     createdAt: undefined;
     updatedAt: undefined;
 }, "">;
--- src/schemas/participant.ts
+++ src/schemas/participant.ts
@@ -2,10 +2,17 @@ import * as yup from "yup";
 import { timestampSchema } from "./utils/timestamp";
 
 export const participantSchema = yup.object({
+  id: yup.string(),
   userUids: yup.array().of(yup.string().required()).required(),
   displayName: yup.string().required(),
   photoURL: yup.string().optional(),
-  lastLocation: yup.object().optional(),
+  lastLocation: yup.object({
+    latitude: yup.number().required(),
+    longitude: yup.number().required(),
+    timestamp: yup.number().required(),
+  }),
+  approvedAt: timestampSchema.nullable(),
+  rejectedAt: timestampSchema,
   createdAt: timestampSchema,
   updatedAt: timestampSchema,
 });
