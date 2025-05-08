f23d6f257115e1eefdfd01993de0b10a76cdd28e
Michael Dawson <m.dawson@y7mail.com>
2025-05-08 10:49:11 -0500

    Add senderName to message schema

--- src/schemas/message.ts
+++ src/schemas/message.ts
@@ -5,6 +5,7 @@ import { timestampSchema } from "./utils/timestamp";
 export const messageSchema = yup.object({
   id: yup.string(),
   senderId: yup.string().required(),
+  senderName: yup.string().required(),
   recipientId: yup.string().required(),
   message: yup.string().required(),
   createdAt: timestampSchema.optional(),
