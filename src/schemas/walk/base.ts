1e0df5b2a141d72c5556503f0c3de80943cfa083
Michael Dawson <m.dawson@y7mail.com>
2025-05-08 13:18:59 -0500

    Remove rsvpUserUids from walks schema

--- src/schemas/walk/base.ts
+++ src/schemas/walk/base.ts
@@ -21,7 +21,6 @@ export const walkBaseSchema = yup.object({
   id: yup.string(),
   date: timestampSchema.required(),
   active: yup.boolean().required(),
-  rsvpdUserIds: yup.array().of(yup.string().required()),
   invitedUserIds: yup.array().of(yup.string().required()),
   currentLocation: locationSchema,
   startLocation: locationSchema,
