ba75697c8417c87decbe04d085c18a20e85e2b17
Michael Dawson <m.dawson@y7mail.com>
2025-10-12 09:15:39 +1030

    Remove start location from friends walk schema

--- src/schemas/walk/friends.ts
+++ src/schemas/walk/friends.ts
@@ -1,10 +1,8 @@
 import * as yup from "yup";
-import { locationSchema } from "../location";
 import { walkBaseSchema } from "./base";
 
 export const friendsWalkSchema = walkBaseSchema.shape({
   type: yup.mixed<"friends">().oneOf(["friends"]).required(),
-  startLocation: locationSchema.required(),
 });
 
 export type FriendsWalk = yup.InferType<typeof friendsWalkSchema>;
