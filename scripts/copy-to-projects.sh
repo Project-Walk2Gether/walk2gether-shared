6c85ea5773f6f1b1cea80bb3b0f474268cad0699
Michael Dawson <m.dawson@y7mail.com>
2025-11-04 13:58:53 +1000

    Add script to copy lib directory into projects

---
+++ scripts/copy-to-projects.sh
@@ -0,0 +1,6 @@
+echo "Copying lib directory to projects"
+
+cp -r lib ../walk2gether-functions/node_modules/walk2gether-shared/lib
+cp -r lib ../walk2gether-expo/node_modules/walk2gether-shared/lib
+
+echo "Copying complete"
