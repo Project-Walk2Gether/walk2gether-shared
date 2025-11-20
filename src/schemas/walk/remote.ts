b1d3829c16297a7f26ca97a8942aa28e1dd1c6e5
Michael Dawson <m.dawson@y7mail.com>
2025-11-20 10:04:05 -0600

    Update walk types

--- src/schemas/walk/remote.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-import { walkBaseSchema } from "./base";
-
-export const remoteWalkSchema = walkBaseSchema.shape({
-  type: yup.mixed<"remote">().oneOf(["remote"]).required(),
-});
-
-export type RemoteWalk = yup.InferType<typeof remoteWalkSchema>;
