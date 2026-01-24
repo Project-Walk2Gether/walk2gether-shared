e277b85565943ff90c141acdacd3c2b8e7bb4ffd
Michael Dawson <m.dawson@y7mail.com>
2026-01-24 17:16:32 -0600

    Add country to location schema

--- src/schemas/location.ts
+++ src/schemas/location.ts
@@ -7,7 +7,7 @@ export const locationSchema = yup.object({
   name: yup.string(),
   displayName: yup.string().required(), // City, state, country format for admin display
   city: yup.string().required(),
-  // address: yup.string().required(),
+  country: yup.string().required(),
   createdAt: timestampSchema,
   updatedAt: timestampSchema,
   placeId: yup.string(),
