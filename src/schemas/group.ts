4e634ce309e99a34e4f6b08708328e9d47c746ad
Michael Dawson <m.dawson@y7mail.com>
2026-02-12 16:13:50 -0600

    Add id to group schema

--- src/schemas/group.ts
+++ src/schemas/group.ts
@@ -1,6 +1,7 @@
 import * as yup from "yup";
 
 export const groupSchema = yup.object({
+  id: yup.string(),
   name: yup.string().required(),
   invitationCode: yup.string().required(),
   memberUids: yup.array().of(yup.string().required()).required(),
